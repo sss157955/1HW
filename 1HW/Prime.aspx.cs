f78bd39bf9a072d1e14372770374a93a191bfea2
益宏 劉 <107111224@mail.oit.edu.tw>
2021-03-16 17:50:57 +0800

    two

---
+++ 1HW/Prime.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _1HW
+{
+    public partial class Prime : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int prime = 19;
+            int count = 0;
+            for (int i= 1; i <= prime; i++)
+            {
+                if (prime %i== 0)
+                    {
+                        count += 1;
+                    }
+            }
+            if (count == 2)
+            {
+                Response.Write("YES");
+            }
+            else
+            {
+                Response.Write("NO");
+            }
+        }
+    }
+}
